e591bbb552c073620bae13d83ea48ed419f8a214
gcsikos <csiki123@gmail.com>
2020-11-27 21:24:20 +0100

    Added structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent  },
+  { path: 'login', component: LoginComponent },
+  { path: 'todos', component: ListTodoComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'add', component: AddTodoComponent},
+
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListTodoComponent,
+    AddTodoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
